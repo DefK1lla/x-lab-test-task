8d4d94dba75056018528b14e46f834b47bda2b1b
Sedrak Adoyan <set.adoyan@gmail.com>
2023-06-06 11:59:49 +0400

    feat: add address search page text

--- src/pages/AdressSearch/AddressSearch.tsx
+++ src/pages/AdressSearch/AddressSearch.tsx
@@ -1,8 +1,12 @@
 import { AddressSearchForm } from 'features'
 
+import s from './addressSearch.module.scss'
+
 const AddressSearchPage = () => {
   return (
-    <div>
+    <div className={s.wrapper}>
+      <h1 className={s.title}>Поиск адресов</h1>
+      <p className={s.text}>Введите интересующий вас адрес</p>
       <AddressSearchForm onSubmit={console.log} />
     </div>
   )
