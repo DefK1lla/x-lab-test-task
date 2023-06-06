3e948a173c4a3fd5338591386955b0babbdd6f26
Sedrak Adoyan <set.adoyan@gmail.com>
2023-06-06 19:52:09 +0400

    feat: menu item highlight

--- src/features/SideMenu/SideMenu.tsx
+++ src/features/SideMenu/SideMenu.tsx
@@ -4,10 +4,11 @@ import ROUTES from 'shared/constants/routes'
 import s from './sideMenu.module.scss'
 import { useWindowDimensions } from 'shared/hooks/use-window-dimensions'
 import { SCREEN_MD } from 'shared/constants/breakpoints'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 const SideMenu = () => {
   const { width } = useWindowDimensions()
+  const location = useLocation()
   return (
     <div className={s.wrapper}>
       {width < SCREEN_MD && (
@@ -17,7 +18,11 @@ const SideMenu = () => {
       )}
       <h2 className={s.title}>Меню</h2>
       {ROUTES.map(r => (
-        <MenuItem key={r.label} {...r} />
+        <MenuItem
+          key={r.label}
+          {...r}
+          isActive={r => location.pathname === r.href}
+        />
       ))}
     </div>
   )
